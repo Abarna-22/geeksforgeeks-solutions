f9e591bc4c7de18317a9d0caa6dfc082437ab4c9
Abarna-22 <abarnasupramani@gmail.com>
2026-03-11 21:21:32 +0530

    Fix formatting and clean up missingnumber.java

--- missingnumber.java
+++ missingnumber.java
@@ -1,11 +1,6 @@
 class missingnumber{
  public static long  missingNum(long[] arr, long size) {
-<<<<<<< HEAD:Day-1/missingno.java
-    
      long n=size+1; 
-=======
-     long n=size+1; //+1 for expected size is 1,2,3,4,5 but the array is 1,2,3,4
->>>>>>> 7fd8506 (Updated project files):missingnumber.java
      long sum=n*(n+1)/2;
      long arraysum=0;
     for(int i=0;i<size;i++)
@@ -23,3 +18,4 @@ public static void main(String[] args)
     System.out.println(missingNum(arr,size));
 }
 }
+
